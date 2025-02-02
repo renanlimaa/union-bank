86b342a0d78799b3cadc53a6bd22e3ee44dcb1d5
renan <renanlimaa2005@hotmail.com>
2025-02-01 21:02:59 -0300

    Subir arquivos do repositório local para repositório remoto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const accordions = document.querySelectorAll('.accordion');
+accordions.forEach(accordion => {
+    accordion.addEventListener('click', () => {
+        const body = accordion.querySelector('.accordion-body');
+        body.classList.toggle('active');
+    })
+})
